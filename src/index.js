c2e2a7985dd941e2a47984604e4af2dced9df207
Andrey Hancharou <mr_dron@tut.by>
2019-03-21 01:12:53 +0300

    feat: complete task brackets 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let strlength = str.length;
+
+  for (let i = 0; i < strlength; i++) {
+    for (let a=0; a < bracketsConfig.length; a++) {
+      let pattern = bracketsConfig[a].join('');
+      str = str.replace(pattern, '');
+    }
+  }
+
+  if (str.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
